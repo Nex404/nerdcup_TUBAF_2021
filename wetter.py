9c0adb083f32b08736a8b953afe543d42a32724e
Hernán Valdés <hfvaldesg@gmail.com>
2021-06-11 14:39:07 +0200

    Add new data from covid19

--- wetter.py
+++ wetter.py
@@ -5,5 +5,7 @@ filepath = "data/Wetter/Wetterdaten_HumboldtBau.csv"
 
 data = pd.read_csv(filepath, delimiter=",", header=0)
 
-data = data.drop(columns=["latitude", "longitude", "elevation", "Light", "Temp_680", "hum_680"])
+data = data.drop(columns=["latitude", "longitude", "elevation", "Light", "TEMP_680", "Hum_680"])
+print(data.iloc[0])
+
 print(data.describe())
