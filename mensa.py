42450179b43ce1ad9a0d418e7cf6223238cd4e9c
Hernán Valdés <hfvaldesg@gmail.com>
2021-06-12 08:11:35 +0200

    small changes

--- mensa.py
+++ mensa.py
@@ -9,6 +9,7 @@ def main():
     all_mensa_data = list()
     for mensa in mensa_data:
         data = pd.read_csv(mensa["filename"], sep=",", header=0)
+        data["Woche"] = data["Woche"].astype(str)
         all_mensa_data.append(data)
     
     all_mensa = pd.concat(all_mensa_data, ignore_index=True)
@@ -16,36 +17,45 @@ def main():
     # Covid dataframe
     covid_csv = "./data/covid19/Mittelsachsen.csv"
     covid_data = pd.read_csv(covid_csv, sep=",", header=0)
+    covid_data["Woche"] = data["Woche"].astype(str)
     
     # weather dataframe
     weather_csv = "./data/Wetter/wetter_mod6.csv"
     weather_data = pd.read_csv(weather_csv, sep=",", header=0)
+    weather_data["Woche"] = weather_data["Woche"].astype(str)
 
     # Join the mensa info with the covid information
     merged_df = pd.merge(all_mensa, covid_data, on="Woche")
     
-    # hier noch mal gucken ob alles passt
-    merged_df = pd.merge(merged_df, weather_data, on="Woche")
-    print(merged_df.head())
-    # Group by week
-    grouped_df = merged_df.groupby(by=["Woche"])
+    # merged_df = pd.merge(merged_df, weather_data, on="Woche")
+    # grouped_df = merged_df.groupby(by=["Woche"])
 
     total_values = list()
     for name in mensa_names:
         x = merged_df[(merged_df["Name"] == name)]
         x_total = x.groupby(by=["Woche", "Name"])["Anzahl"].sum()
         x_total = x_total.reset_index()
+        print(x_total)
         total_values.append(x_total)
 
     colors = ['#E69F00', '#56B4E9', '#F0E442', '#009E73', '#D55E00']
-    
+
     x_values = [temp_df["Woche"] for temp_df in total_values]
     y_values = [temp_df["Anzahl"] for temp_df in total_values]
+    
+    fig, ax = plt.subplots()
 
-    # plt.hist([x1, x2, x3, x4, x5], color=colors, label=mensa_names, stacked=True)
+    for index, value in enumerate(mensa_names):
+        ax.bar(x=x_values[index], height=y_values[index], color=colors[index], width=0.3, align="edge", label=value)
 
-    # plt.legend()
-    # plt.savefig("test.png")
+    # ax.set_xticklabels(mensa_names, fontdict=None, minor=False)
+    # ax.xticks(rotation=45)    
+
+    ax.legend()
+    ax.set_ylabel('Scores')
+    fig.tight_layout()
+
+    plt.savefig("test.png")
 
 if __name__ == "__main__":
     main()
