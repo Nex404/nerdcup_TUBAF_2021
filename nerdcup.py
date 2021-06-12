6c2ac25f6358ebdf2b513164431028d68e782c5c
Hernán Valdés <hfvaldesg@gmail.com>
2021-06-12 13:02:13 +0200

    Update to last state

--- nerdcup.py
+++ nerdcup.py
@@ -2,21 +2,22 @@ import requests
 import pandas as pd
 from mensa import preprocessing
 import matplotlib.pyplot as plt
+from utils import *
 
 def mensa_isolated(mensa_list, output):
     for index, mensa in enumerate(mensa_list):
         mensa.to_csv("test.csv")
 
         # total_values[0]["Woche"].plt
-        anzahl = mensa[mensa["year"] == 2020]["Anzahl"].values
-        covid = mensa[mensa["year"] == 2020]["COVID"].values
-        woche = mensa[mensa["year"] == 2020]["Woche"].values
+        anzahl = mensa["Anzahl"].values
+        covid = mensa["COVID"].values
+        woche = mensa["Woche"].values
 
         fig, ax1 = plt.subplots(figsize=(20, 5))
         plt.xticks(rotation=45)
-
+        ax1.set_title(f"{mensa_names[index]}")
         ax1.set_xlabel("Woche")
-        ax1.set_ylabel("Pizza", color="blue")
+        ax1.set_ylabel("Einkäufe", color="blue")
         ax1.bar(height=anzahl, x=woche, width=0.8, bottom=None, align='center', color="blue")
 
         ax2 = ax1.twinx()
@@ -29,7 +30,7 @@ def mensa_isolated(mensa_list, output):
 def main():
     total_values = preprocessing()
     
-    mensa_isolated(total_values, "./images/isolated")
+    mensa_isolated(total_values, "./images/")
     
 
 if __name__ == "__main__":
